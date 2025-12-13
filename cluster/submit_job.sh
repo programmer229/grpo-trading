9f7d4514ae9dfb4d5d406393d4f742d86fdf2806
Daniil Dzenhaliou <ddengalev@Daniils-MacBook-Air.local>
2025-12-13 14:09:06 +0100

    Fix bug

--- cluster/submit_job.sh
+++ cluster/submit_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=logs/%x-%j.err
 #SBATCH --gpus=8
 #SBATCH --time=04:00:00
-#SBATCH --partition=train
+#SBATCH --partition=training
 #SBATCH --container-image=slimerl/slime:latest
 #SBATCH --container-mounts=./:/workspace
 
